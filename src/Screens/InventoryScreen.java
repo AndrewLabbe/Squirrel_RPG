1d608969b75058199d16e03f0c2303d69c0b34bd
AlecGoriup <113146110+AlecGoriup@users.noreply.github.com>
2022-10-12 05:10:22 -0400

    version1 of inventory

--- src/Screens/InventoryScreen.java
+++ src/Screens/InventoryScreen.java
@@ -14,7 +14,6 @@ import java.awt.*;
 public class InventoryScreen extends Screen {
     protected ScreenCoordinator screenCoordinator;
     protected int currentItem = 0; // current item position
-//    protected int menuItemSelected = -1;
     protected SpriteFont inventory;
     protected Map background;
     protected Stopwatch keyTimer = new Stopwatch();
@@ -22,9 +21,10 @@ public class InventoryScreen extends Screen {
     protected KeyLocker keyLocker = new KeyLocker();
     
     private final Key escKey = Key.ESC;
-    protected int yPos, range, itemBoxSize, next;
-    protected float space;
-    protected GameObject.Rectangle box1, box2, box3, box4, box5;
+    protected int yPos, range, itemBoxSize, base;
+    protected float space, next;
+    protected GameObject.Rectangle box1, box2, box3, box4, box5, invTable
+    								,info1, info2, info3, info4, info5;
 
     public InventoryScreen(ScreenCoordinator screenCoordinator) {
         this.screenCoordinator = screenCoordinator;
@@ -32,21 +32,74 @@ public class InventoryScreen extends Screen {
 
     @Override
     public void initialize() {
-        inventory = new SpriteFont("Inventory", 5, 5, "Comic Sans", 12, new Color(49, 207, 240));
-        inventory.setOutlineColor(Color.black);
-        inventory.setOutlineThickness(1);
-
-    	
     	range = Config.GAME_WINDOW_HEIGHT/5; //range for item box and equal spaces
-    	itemBoxSize = range * 3/4;
+    	itemBoxSize = range * 1/2;
     	space = (range * 1/4) /2;
+    	next = itemBoxSize + space;
+    	base = range * 3/4;
+    	
+    	// inventory table
+    	invTable = new GameObject.Rectangle(2*space, 2*space, Config.GAME_WINDOW_WIDTH*1/2, Config.GAME_WINDOW_HEIGHT*3/4);
+    	invTable.setColor(new Color(77, 75, 115));
+    	invTable.setBorderColor(Color.black);
+    	invTable.setBorderThickness(3);
+    	
+        inventory = new SpriteFont("Inventory", itemBoxSize, base*3/4, "Comic Sans", 32, new Color(49, 207, 240));
+        inventory.setOutlineColor(Color.black);
+        inventory.setOutlineThickness(3);
     	
 //    	Rectangle box = new Rectangle(space, yPos+space, itemBoxSize, itemBoxSize);  
-    	box1 = new GameObject.Rectangle(space, space, itemBoxSize, itemBoxSize);
-    	box2 = new GameObject.Rectangle(space, range+space, itemBoxSize, itemBoxSize);
-    	box3 = new GameObject.Rectangle(space, (2*range)+space, itemBoxSize, itemBoxSize);
-    	box4 = new GameObject.Rectangle(space, (3*range)+space, itemBoxSize, itemBoxSize);
-    	box5 = new GameObject.Rectangle(space, (4*range)+space, itemBoxSize, itemBoxSize);
+    	box1 = new GameObject.Rectangle(itemBoxSize, base, itemBoxSize, itemBoxSize);
+    	box2 = new GameObject.Rectangle(itemBoxSize, base+(1*next), itemBoxSize, itemBoxSize);
+    	box3 = new GameObject.Rectangle(itemBoxSize, base+(2*next), itemBoxSize, itemBoxSize);
+    	box4 = new GameObject.Rectangle(itemBoxSize, base+(3*next), itemBoxSize, itemBoxSize);
+    	box5 = new GameObject.Rectangle(itemBoxSize, base+(4*next), itemBoxSize, itemBoxSize);
+    	
+    	box1.setColor(new Color(90, 89, 110));
+    	box1.setBorderColor(Color.black);
+    	box1.setBorderThickness(3);
+    	
+    	box2.setColor(new Color(90, 89, 110));
+    	box2.setBorderColor(Color.black);
+    	box2.setBorderThickness(3);
+    	
+    	box3.setColor(new Color(90, 89, 110));
+    	box3.setBorderColor(Color.black);
+    	box3.setBorderThickness(3);
+    	
+    	box4.setColor(new Color(90, 89, 110));
+    	box4.setBorderColor(Color.black);
+    	box4.setBorderThickness(3);
+    	
+    	box5.setColor(new Color(90, 89, 110));
+    	box5.setBorderColor(Color.black);
+    	box5.setBorderThickness(3);
+    	
+    	info1 = new GameObject.Rectangle(next+itemBoxSize, base, itemBoxSize*4, itemBoxSize);
+    	info2 = new GameObject.Rectangle(next+itemBoxSize, base+(1*next), itemBoxSize*4, itemBoxSize);
+    	info3 = new GameObject.Rectangle(next+itemBoxSize, base+(2*next), itemBoxSize*4, itemBoxSize);
+    	info4 = new GameObject.Rectangle(next+itemBoxSize, base+(3*next), itemBoxSize*4, itemBoxSize);
+    	info5 = new GameObject.Rectangle(next+itemBoxSize, base+(4*next), itemBoxSize*4, itemBoxSize);
+    	
+//    	info1.setColor(new Color(90, 89, 110));
+    	info1.setBorderColor(Color.black);
+    	info1.setBorderThickness(3);
+    	
+//    	info2.setColor(new Color(90, 89, 110));
+    	info2.setBorderColor(Color.black);
+    	info2.setBorderThickness(3);
+    	
+//    	info3.setColor(new Color(90, 89, 110));
+    	info3.setBorderColor(Color.black);
+    	info3.setBorderThickness(3);
+    	
+//    	info4.setColor(new Color(90, 89, 110));
+    	info4.setBorderColor(Color.black);
+    	info4.setBorderThickness(3);
+    	
+//    	info5.setColor(new Color(90, 89, 110));
+    	info5.setBorderColor(Color.black);
+    	info5.setBorderThickness(3);
     	
     	
 //    	for(yPos = 0; yPos < Config.GAME_WINDOW_HEIGHT; yPos = yPos + range) {
@@ -77,16 +130,7 @@ public class InventoryScreen extends Screen {
             keyTimer.reset();
             currentItem--;
         }
-        
 
-        	
-
-//        // if down is pressed on last menu item or up is pressed on first menu item, "loop" the selection back around to the beginning/end
-//        if (currentItem > 1) {
-//            currentItem = 0;
-//        } else if (currentItem < 0) {
-//            currentItem = 1;
-//        }
 
         // sets color of spritefont text based on which menu item is being hovered
         if (currentItem == 0) {
@@ -128,6 +172,7 @@ public class InventoryScreen extends Screen {
 
     public void draw(GraphicsHandler graphicsHandler) {
         background.draw(graphicsHandler);
+        invTable.draw(graphicsHandler);
         inventory.draw(graphicsHandler);
         
         box1.draw(graphicsHandler);
@@ -136,11 +181,11 @@ public class InventoryScreen extends Screen {
         box4.draw(graphicsHandler);
         box5.draw(graphicsHandler);
         
-//        graphicsHandler.drawFilledRectangleWithBorder(space, space, itemBoxSize, itemBoxSize, new Color(90, 89, 110), Color.black, 2);
-//        graphicsHandler.drawFilledRectangleWithBorder(space, range+space, itemBoxSize, itemBoxSize, new Color(90, 89, 110), Color.black, 2);
-//        graphicsHandler.drawFilledRectangleWithBorder(space, (2*range)+space, itemBoxSize, itemBoxSize, new Color(90, 89, 110), Color.black, 2);
-//        graphicsHandler.drawFilledRectangleWithBorder(space, (3*range)+space, itemBoxSize, itemBoxSize, new Color(90, 89, 110), Color.black, 2);
-//        graphicsHandler.drawFilledRectangleWithBorder(space, (4*range)+space, itemBoxSize, itemBoxSize, new Color(90, 89, 110), Color.black, 2);
+        info1.draw(graphicsHandler);
+        info2.draw(graphicsHandler);
+        info3.draw(graphicsHandler);
+        info4.draw(graphicsHandler);
+        info5.draw(graphicsHandler);
     }
 
 //    public int getMenuItemSelected() {
