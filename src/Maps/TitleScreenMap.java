1ae918908e8f59829e79c56ac1b302a7f1f019ce
Garrett <ggoldbergtwin@gmail.com>
2022-11-07 01:49:29 -0500

    Redid Title Screen to Include New Graphics

--- src/Maps/TitleScreenMap.java
+++ src/Maps/TitleScreenMap.java
@@ -6,6 +6,7 @@ import GameObject.ImageEffect;
 import GameObject.Sprite;
 import Level.Map;
 import Tilesets.CommonTileset;
+import Tilesets.PathingTileset;
 import Utils.Colors;
 import Utils.Point;
 
@@ -15,11 +16,11 @@ public class TitleScreenMap extends Map {
     private Sprite cat;
 
     public TitleScreenMap() {
-        super("title_screen_map.txt", new CommonTileset());
-        Point catLocation = getMapTile(8, 5).getLocation().subtractX(6).subtractY(7);
+        super("title_screen_map.txt", new PathingTileset());
+        Point catLocation = getMapTile(8, 3).getLocation().subtractX(6).subtractY(7);
         cat = new Sprite(ImageLoader.loadSubImage("Cat.png", Colors.MAGENTA, 0, 0, 24, 24));
         cat.setScale(3);
-        cat.setImageEffect(ImageEffect.FLIP_HORIZONTAL);
+//        cat.setImageEffect(ImageEffect.FLIP_HORIZONTAL);
         cat.setLocation(catLocation.x, catLocation.y);
     }
 
