749afeb5333370c5ef124e89070f9efb5d3f59ae
Garrett <ggoldbergtwin@gmail.com>
2022-11-09 01:33:23 -0500

    Added Idle Animation for Sprite

--- src/Players/Squirrel.java
+++ src/Players/Squirrel.java
@@ -33,18 +33,63 @@ public class Squirrel extends Player {
 	public HashMap<String, Frame[]> loadAnimations(SpriteSheet spriteSheet) {
 		return new HashMap<String, Frame[]>() {{
 			put("STAND_RIGHT", new Frame[] {
-					new FrameBuilder(spriteSheet.getSprite(0, 0))
+					new FrameBuilder(spriteSheet.getSprite(0, 0), 100)
 					.withScale(3)
 					.withBounds(6, 12, 12, 7)
-					.build()
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 1), 100)
+					.withScale(3)
+					.withBounds(6, 12, 12, 7)
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 2), 100)
+					.withScale(3)
+					.withBounds(6, 12, 12, 7)
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 3), 100)
+					.withScale(3)
+					.withBounds(6, 12, 12, 7)
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 4), 100)
+					.withScale(3)
+					.withBounds(6, 12, 12, 7)
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 5), 100)
+					.withScale(3)
+					.withBounds(6, 12, 12, 7)
+					.build(),
 			});
 
 			put("STAND_LEFT", new Frame[] {
-					new FrameBuilder(spriteSheet.getSprite(0, 0))
+					new FrameBuilder(spriteSheet.getSprite(0, 0), 100)
 					.withScale(3)
 					.withImageEffect(ImageEffect.FLIP_HORIZONTAL)
 					.withBounds(6, 12, 12, 7)
-					.build()
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 1), 100)
+					.withScale(3)
+					.withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+					.withBounds(6, 12, 12, 7)
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 2), 100)
+					.withScale(3)
+					.withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+					.withBounds(6, 12, 12, 7)
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 3), 100)
+					.withScale(3)
+					.withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+					.withBounds(6, 12, 12, 7)
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 4), 100)
+					.withScale(3)
+					.withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+					.withBounds(6, 12, 12, 7)
+					.build(),
+					new FrameBuilder(spriteSheet.getSprite(0, 5), 100)
+					.withScale(3)
+					.withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+					.withBounds(6, 12, 12, 7)
+					.build(),
 			});
 
 			put("WALK_RIGHT", new Frame[] {
