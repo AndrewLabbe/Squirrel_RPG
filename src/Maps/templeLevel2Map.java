f1e94149daa53c6be7d65650ce22ea099b29544d
Garrett <ggoldbergtwin@gmail.com>
2022-11-28 19:02:00 -0500

    Fixed Swimming Triggers in Lv2

--- src/Maps/templeLevel2Map.java
+++ src/Maps/templeLevel2Map.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import Level.Map;
 import Level.Spawner;
 import Level.Trigger;
+import Scripts.TestMap.TempleLandScript;
 import Scripts.TestMap.TempleLevel2Script;
 import Scripts.TestMap.TempleSwimScript;
 import Spawners.BasicSpawner;
@@ -42,28 +43,29 @@ public class templeLevel2Map extends Map {
 		triggers.add(new Trigger(90, 1400, 1350, 10, new TempleSwimScript(), "templeSwam"));
 		
 		// Platform A (Top Left)
-		triggers.add(new Trigger(345, 450, 170, 10, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(345, 600, 170, 10, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(345, 450, 10, 150, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(515, 450, 10, 150, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(345, 450, 170, 1, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(345, 600, 170, 1, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(345, 450, 1, 150, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(515, 450, 1, 155, new TempleSwimScript(), "templeSwam"));
+		
 		
 		// Platform B (Mid Right)
-		triggers.add(new Trigger(1020, 625, 170, 10, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(1020, 785, 170, 10, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(1020, 625, 10, 170, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(1190, 625, 10, 170, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(1020, 625, 170, 1, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(1020, 790, 170, 1, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(1020, 625, 1, 170, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(1190, 625, 1, 170, new TempleSwimScript(), "templeSwam"));
 		
 		// Platform C (Mid Left)
-		triggers.add(new Trigger(345, 910, 170, 10, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(345, 1080, 170, 10, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(345, 910, 10, 170, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(515, 910, 10, 170, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(345, 910, 170, 1, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(345, 1080, 175, 1, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(345, 910, 1, 170, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(515, 910, 1, 170, new TempleSwimScript(), "templeSwam"));
 		
 		// Platform D (bottom Right)
-		triggers.add(new Trigger(1020, 1110, 170, 10, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(1020, 1275, 170, 10, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(1020, 1110, 10, 170, new TempleSwimScript(), "templeSwam"));
-		triggers.add(new Trigger(1190, 1110, 10, 170, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(1020, 1110, 170, 1, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(1020, 1275, 170, 1, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(1020, 1110, 1, 170, new TempleSwimScript(), "templeSwam"));
+		triggers.add(new Trigger(1190, 1110, 1, 170, new TempleSwimScript(), "templeSwam"));
 		
 		// Go to Level 3
 		triggers.add(new Trigger(670, 1625, 200, 10, new TempleLevel2Script(), "hasEnteredLevel3"));
