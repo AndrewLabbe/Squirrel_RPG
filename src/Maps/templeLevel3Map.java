70803ac19ded88f91d5e3ec372132adea811772a
Garrett <ggoldbergtwin@gmail.com>
2022-11-30 02:16:15 -0500

    Added Intro Trigger Script to Level 3

--- src/Maps/templeLevel3Map.java
+++ src/Maps/templeLevel3Map.java
@@ -11,6 +11,7 @@ import Level.EnhancedMapTile;
 import Level.Map;
 import Tilesets.temple3Tileset;
 import Level.NPC;
+import Level.Trigger;
 import NPCs.Chest;
 import NPCs.Fox;
 import NPCs.Girlfriend;
@@ -21,77 +22,86 @@ import Scripts.TestMap.ChestUnlockScriptGenesis;
 import Scripts.TestMap.ChestUnlockScriptRetribution;
 import Scripts.TestMap.ChestUnlockScriptTemple;
 import Scripts.TestMap.FoxScript;
+import Scripts.TestMap.TempleLevel3IntroScript;
 import Scripts.TestMap.WalrusScript;
 import Tilesets.templeTileset;
 import Utils.Point;
 
 // Represents the Map for the Temple
 public class templeLevel3Map extends Map {
-	
+
 	public templeLevel3Map() {
 		super("temple_Level3.txt", new temple3Tileset());
 		this.playerStartPosition = getMapTile(7, 0).getLocation();
 	}
-	
+
 	@Override
-	 public ArrayList<NPC> loadNPCs() {
-	     ArrayList<NPC> npcs = new ArrayList<>();
-	     
-	     
-	     Point point = new Point(getMapTile(2,2).getX() + 30, getMapTile(2,2).getY() + 30);
-	     Chest chestDestiny = new Chest(3, point, "hasOpenedChestTempleLvl3");  
-	     chestDestiny.setInteractScript(new ChestUnlockScriptTemple()); 
-	     chestDestiny.getInteractScript().setMap(this);
-	     npcs.add(chestDestiny);
-	     
-	     return npcs; 
-	 } 
-	
-	 @Override
-	 public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
-	     ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
-	     //Enhanced lava tiles 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(2,4).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(1,5).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(2,7).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(1,9).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(3,9).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(1,11).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(2,12).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(1,13).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(7,3).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(6,6).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(6,8).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(5,9).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(6,9).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(5,10).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(6,12).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(5,13).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(7,14).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(7,4).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(8,10).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(9,11).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(9,12).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(9,14).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(12,13).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(10,11).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(12,9).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(3,13).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(11,8).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(12,5).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(11,6).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(11,3).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(12,1).getLocation(), "LavaTile3.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(4,12).getLocation(), "LavaTile2.png")); 
-	     enhancedMapTiles.add(new LavaTile(getMapTile(3,12).getLocation(), "LavaTile3.png")); 
-	     
-	     //Enhanced projectile trap tiles 
-	     enhancedMapTiles.add(new ProjectileTrapTile(getMapTile(1,7).getLocation())); 
-	     enhancedMapTiles.add(new ProjectileTrapTile(getMapTile(1,3).getLocation())); 
-	     enhancedMapTiles.add(new ProjectileTrapTile(getMapTile(10,3).getLocation())); 
-	     enhancedMapTiles.add(new ProjectileTrapTile(getMapTile(10,7).getLocation())); 
-	     
-	     return enhancedMapTiles;
-	 }
-	
+	public ArrayList<NPC> loadNPCs() {
+		ArrayList<NPC> npcs = new ArrayList<>();
+
+
+		Point point = new Point(getMapTile(2,2).getX() + 30, getMapTile(2,2).getY() + 30);
+		Chest chestDestiny = new Chest(3, point, "hasOpenedChestTempleLvl3");  
+		chestDestiny.setInteractScript(new ChestUnlockScriptTemple()); 
+		chestDestiny.getInteractScript().setMap(this);
+		npcs.add(chestDestiny);
+
+		return npcs; 
+	} 
+
+	@Override
+	public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
+		ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
+		//Enhanced lava tiles 
+		enhancedMapTiles.add(new LavaTile(getMapTile(2,4).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(1,5).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(2,7).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(1,9).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(3,9).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(1,11).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(2,12).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(1,13).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(7,3).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(6,6).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(6,8).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(5,9).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(6,9).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(5,10).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(6,12).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(5,13).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(7,14).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(7,4).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(8,10).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(9,11).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(9,12).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(9,14).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(12,13).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(10,11).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(12,9).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(3,13).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(11,8).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(12,5).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(11,6).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(11,3).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(12,1).getLocation(), "LavaTile3.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(4,12).getLocation(), "LavaTile2.png")); 
+		enhancedMapTiles.add(new LavaTile(getMapTile(3,12).getLocation(), "LavaTile3.png")); 
+
+		//Enhanced projectile trap tiles 
+		enhancedMapTiles.add(new ProjectileTrapTile(getMapTile(1,7).getLocation())); 
+		enhancedMapTiles.add(new ProjectileTrapTile(getMapTile(1,3).getLocation())); 
+		enhancedMapTiles.add(new ProjectileTrapTile(getMapTile(10,3).getLocation())); 
+		enhancedMapTiles.add(new ProjectileTrapTile(getMapTile(10,7).getLocation())); 
+
+		return enhancedMapTiles;
+	}
+
+	public ArrayList<Trigger> loadTriggers() {
+		ArrayList<Trigger> triggers = new ArrayList<>();
+
+		triggers.add(new Trigger(550, 200, 250, 10, new TempleLevel3IntroScript(), "hasEnteredLevel3"));
+		
+		return triggers;
+	}
+
 }
---
+++ src/Scripts/TestMap/TempleLevel3IntroScript.java
@@ -0,0 +1,40 @@
+package Scripts.TestMap;
+
+import Level.Script;
+import Level.ScriptState;
+
+public class TempleLevel3IntroScript extends Script {
+
+	@Override
+	protected void setup() {
+		lockPlayer();
+		showTextbox();
+		if(!isFlagSet("hasEnteredLevel3")) {
+			addTextToTextboxQueue("I tihink I'm close...");
+			addTextToTextboxQueue("I will find this stranger and \n make them pay.");
+			addTextToTextboxQueue("Is this room littered with traps?");
+			addTextToTextboxQueue("I need to be careful...");
+			addTextToTextboxQueue("I need to get to that chest and \nEnter the last room...");
+		}
+		
+	}
+
+	@Override
+	protected void cleanup() {
+		setFlag("hasEnteredLevel3");
+		unlockPlayer();
+		hideTextbox();
+		
+	}
+
+	@Override
+	protected ScriptState execute() {
+		start();
+        if (!isTextboxQueueEmpty()) {
+            return ScriptState.RUNNING;
+        }
+        end();
+        return ScriptState.COMPLETED;
+	}
+
+}
