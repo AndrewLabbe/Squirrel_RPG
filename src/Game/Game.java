53f87ba0a6bb92d3a01927cae7f68e1007a3b914
Garrett Goldberg <98359240+gegoldberg@users.noreply.github.com>
2022-11-30 22:27:49 -0500

    Backup Prior to Alec Shopkeep Push

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -6,7 +6,7 @@ import Engine.ScreenManager;
 /*
  * The game starts here
  * This class just starts up a GameWindow and attaches the ScreenCoordinator to the ScreenManager instance in the GameWindow
- * From this point on the ScreenCoordinator c
+ * From this point on the ScreenCoordinator
  */
 
 // Game created by Phil, Andrew L, Andrew K, Alec G, and Mica
